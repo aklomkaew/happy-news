e38753744c13dbe033bbfffb8db51b775324b86b
Atchima Klomkaew <aklomkaew@gmail.com>
2020-03-28 16:52:19 -0500

    Add basic backend using Flask

---
+++ happy-news-backend/src/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+users = []
+
+# HomePage
+@app.route("/")
+def hello():
+    return "hello\n"
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json['username']
+    if username in users:
+        abort(401)
+    else:
+        users.append(username)
+        return 'Successfully logged in'
+
+if __name__ == '__main__':
+    app.run(debug=True)
