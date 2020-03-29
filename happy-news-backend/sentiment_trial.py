971a3a160086d7a355c44d8143b83d6ae116a4d3
Phiwat Klomkaew <PhiwatK@Phiwats-MacBook.local>
2020-03-28 20:13:02 -0500

    changed functions from dominant language to sentiment detection

--- happy-news-backend/sentiment_trial.py
+++ happy-news-backend/sentiment_trial.py
@@ -2,8 +2,16 @@ import boto3
 import json
 
 comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
+                
 text = "It is raining today in Seattle"
 
-print('Calling DetectDominantLanguage')
-print(json.dumps(comprehend.detect_dominant_language(Text = text), sort_keys=True, indent=4))
-print("End of DetectDominantLanguage\n")
+print('Calling DetectSentiment')
+print(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+print('End of DetectSentiment\n')
+
+
+
+# aws comprehend detect-sentiment \
+#     --region "us-west-2" \
+#     --language-code "en" \
+#     --text "It is raining today in Seattle."
