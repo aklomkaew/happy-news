f86ade651cc3cb020f1d60184de6b9d28977330a
Atchima Klomkaew <aklomkaew@gmail.com>
2020-03-28 23:29:53 -0500

    Add function to get articles from the given news_provider

--- happy-news-backend/src/server.py
+++ happy-news-backend/src/server.py
@@ -1,22 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, abort, jsonify
+import newspaper
+from newspaper import Article
 
 app = Flask(__name__)
 
-users = []
+articles = []
 
-# HomePage
-@app.route("/")
-def hello():
-    return "hello\n"
+def getArticles(news_provider):
+    papers = newspaper.build('https://' + news_provider + '.com')
+    for a in papers.articles:
+        articles.append({
+            'title': a.title,
+            'authors': a.authors,
+            'publish_date': a.publish_date,
+            'summary': a.summary,
+            'url': a.url,
+            'image': a.top_image,
+            'text': a.text,
+        })
 
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.json['username']
-    if username in users:
+@app.route("/scores", methods=["POST"])
+def getScores():
+    news_provider = request.json.get('news_provider', None)
+    if news_provider is None:
         abort(401)
     else:
-        users.append(username)
-        return 'Successfully logged in'
+        getArticles(news_provider)
+        return jsonify(articles)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
