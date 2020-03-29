a28340bc50becde3a8fd9df84dc8a2b12108c4b5
Atchima Klomkaew <aklomkaew@gmail.com>
2020-03-28 19:48:12 -0500

    Created react native common file and used it in web.

---
+++ packages/common/src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+// import axios from 'axios';
+
+// // public IP address for your computer
+// const serverUrl = 'http://108.227.22.246:5000';
+// const http = axios.create({
+//   baseURL: serverUrl,
+// });
+
+export const App: React.FC = () => {
+  return (
+    <View>
+      <Text>hello from react native web</Text>
+    </View>
+  );
+}
