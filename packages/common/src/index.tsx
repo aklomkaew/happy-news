426cc59cb687af334f0b7bc8236337ad4123775c
Atchima Klomkaew <aklomkaew@gmail.com>
2020-03-29 13:35:01 -0500

    Frontend successfully calls backend

--- packages/common/src/index.tsx
+++ packages/common/src/index.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-// import axios from 'axios';
+import { useState } from 'react';
+import { Text, View, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-// // public IP address for your computer
-// const serverUrl = 'http://108.227.22.246:5000';
-// const http = axios.create({
-//   baseURL: serverUrl,
-// });
+// public IP address for your computer
+const serverUrl = 'http://localhost:5000';
+const http = axios.create({
+  baseURL: serverUrl,
+  withCredentials: true,
+});
+
 
 export const App: React.FC = () => {
+  console.log("boutta up")
+  var body = {"news_provider":"bbc"};
+  http.post('/scores', body).then(function(result) {
+    console.log("yom ywaddup %o", result)
+  });
   return (
     <View>
       <Text>hello from react native web</Text>
+      {/* <Text>{text}</Text> */}
+      {/* <Button title="search" onPress={() => 
+        goSearch(XML.toString(http.post('/scores', "cnn"))}/> */}
     </View>
   );
 }
